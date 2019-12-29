d6861d7247f6d2c1c47cbe074098d27036f8d3a4
Ezequiel Brandan <ezebran@gmail.com>
2019-12-29 17:39:16 -0300

    First commit

---
+++ src/pages/BadgeNew.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import './styles/BadgeNew.css';
+import header from '../images/badge-header.svg';
+import Badge from '../components/Badge';
+import BadgeForm from '../components/BadgeForm';
+import Navbar from '../components/Navbar';
+
+class BadgeNew extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="Logo" />
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <Badge
+                firstName="Ezequiel"
+                lastName="Brandan"
+                twitter="eze_bran"
+                jobTitle="Frontend Engineer"
+                avatarUrl="https://ezebran.github.io/portafolio/img/yo.jpg"
+              />
+            </div>
+
+            <div className="col-6">
+              <BadgeForm />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BadgeNew;
